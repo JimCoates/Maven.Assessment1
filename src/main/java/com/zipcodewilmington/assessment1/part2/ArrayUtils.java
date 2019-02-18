81ddf78b9255a75f1dd31b2445985588fec898e2
James Coates <jamescoates@zipcoders-MacBook-Pro.local>
2019-02-18 11:19:44 -0500

    updated part2

--- src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -11,25 +14,46 @@ public class ArrayUtils {
      * Given an array of objects, named `objectArray`, and an object `objectToCount`, return the number of times the `objectToCount` appears in the `objectArray`
      */
     public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
-        return null;
+
+        Integer count = 0;
+
+        for (int i = 0; i < objectArray.length ; i++) {
+            if(objectArray[i] == objectToCount){
+                count++;
+            }
+
+        }
+
+        return count;
     }
 
     /**
-     * @param objectArray    an array of any type of Object
-     * @param objectToRemove a value to be removed from the `objectArray`
+     * @param inputArray    an array of any type of Object
+     * @param valueToRemove a value to be removed from the `inputArray`
      * @return an array with identical content excluding the specified `objectToRemove`
-     * Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
+     * Given an array of objects, name `inputArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
      */
-    public static Object[] removeValue(Object[] objectArray, Object objectToRemove) {
-        return null;
+    public static Integer[] removeValue(Integer[] inputArray, Integer valueToRemove) {
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(inputArray));
+
+        while(arrayList.contains(valueToRemove)) {
+
+            arrayList.remove(Integer.valueOf(valueToRemove));
+        }
+
+        Integer[] answer = arrayList.toArray(new Integer[arrayList.size()]);
+        return answer;
     }
 
     /**
-     * @param objectArray an array of any type of Object
+     * @param inputArray an array of any type of Object
      * @return the most frequently occurring object in the array
      * given an array of objects, named `objectArray` return the most frequently occuring object in the array
      */
-    public static Object getMostCommon(Object[] objectArray) {
+    public static Integer getMostCommon(Integer[] inputArray) {
+
+
         return null;
     }
 
@@ -44,12 +68,20 @@ public class ArrayUtils {
     }
 
     /**
-     * @param objectArray      an array of any type of Object
-     * @param objectArrayToAdd an array of Objects to add to the first argument
+     * @param array1      an array of any type of Object
+     * @param array2 an array of Objects to add to the first argument
      * @return an array containing all elements in `objectArray` and `objectArrayToAdd`
      * given two arrays `objectArray` and `objectArrayToAdd`, return an array containing all elements in `objectArray` and `objectArrayToAdd`
      */
-    public static Object[] mergeArrays(Object[] objectArray, Object[] objectArrayToAdd) {
-        return null;
+    public static Integer[] mergeArrays(Integer[] array1, Integer[] array2) {
+
+        ArrayList<Integer> list1 = new ArrayList<Integer>(Arrays.asList(array1));
+
+        for (int i = 0; i < array2.length ; i++) {
+            list1.add(array2[i]);
+        }
+        Integer[] answer = list1.toArray(new Integer[list1.size()]);
+        return answer;
+
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/StringUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.assessment1.part2;
 
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
+
 /**
  * Created by leon on 2/16/18.
  */
@@ -11,7 +13,10 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return an array of strings, each element representative of a respective word in the sentence
      */
     public static String[] getWords(String sentence) {
-        return null;
+
+        String[] tempArray = sentence.split(" ");
+
+        return tempArray;
     }
 
 
@@ -21,7 +26,10 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word of the sentence
      */
     public static String getFirstWord(String sentence) {
-        return null;
+
+        String[] answer = getWords(sentence);
+
+        return answer[0] ;
     }
 
     /**
@@ -30,7 +38,9 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order
      */
     public static String reverseFirstWord(String sentence) {
-        return null;
+
+
+       return BasicStringUtils.reverse(getFirstWord(sentence));
     }
 
     /**
@@ -39,7 +49,9 @@ public class StringUtils {
      * given a string containing words delimited by spaces, representative of a sentence, return the first word with identical contents in reverse order with the first character capitalized
      */
     public static String reverseFirstWordThenCamelCase(String sentence) {
-        return null;
+
+
+        return BasicStringUtils.reverseThenCamelCase(getFirstWord(sentence));
     }
 
 
@@ -50,7 +62,16 @@ public class StringUtils {
      * given a string and index, return an identical string excluding the character at the specified index
      */
     public static String removeCharacterAtIndex(String str, int index) {
-        return null;
+        StringBuilder temp = new StringBuilder(str);
+
+        temp.deleteCharAt(index);
+
+
+        String answer = temp.toString();
+
+
+        return answer;
+
     }
 
 }
